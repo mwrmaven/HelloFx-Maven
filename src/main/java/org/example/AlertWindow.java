61618c21b9793bc000658b8df0cf77212f294704
1227156208@qq.com <1227156208@qq.com>
2021-08-29 14:51:23 +0800

    批量修改文件夹下文件名称的功能初步完成，无ui优化

--- src/main/java/org/example/AlertWindow.java
+++ src/main/java/org/example/AlertWindow.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+
 
 
 /**
@@ -16,22 +11,5 @@ import javafx.stage.Stage;
  */
 public class AlertWindow {
 
-    public static void alter(String msg) {
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
 
-        Label label = new Label();
-        label.setText(msg);
-
-        VBox vBox = new VBox();
-        vBox.getChildren().add(label);
-        // 设置居中
-        vBox.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(vBox, 200, 200);
-        stage.setScene(scene);
-        stage.setTitle("提示信息");
-        stage.show();
-
-    }
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,61 +1,125 @@
 package org.example;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 
-import javafx.geometry.Insets;
+import com.mavenr.file.ReName;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.EventListener;
 
 /**
- * Hello world!
+ * @Classname App
+ * @Description 启动类
+ * @Date 2021/8/29 10:02
+ * @author mavenr
  */
 public class App extends Application {
-    private final Desktop desktop = Desktop.getDesktop();
+
     public static void main(String[] args) {
-        launch(App.class, args);
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        Button openButton = new Button("打开文件夹");
-        openButton.setOnAction((ActionEvent e) -> {
-            File file = directoryChooser.showDialog(stage);
-            if (file != null) {
-                System.out.println(file.getAbsoluteFile());
-            } else {
-                System.out.println("请选择文件夹");
-                AlertWindow.alter("请选择文件夹");
+    public void start(Stage primaryStage) throws Exception {
+
+        // 获取屏幕的宽、高
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        double width = bounds.getWidth();
+        double height = bounds.getHeight();
+
+        // 选择文件夹/文件路径，并输入到文本框
+        TextField text = new TextField();
+        // 输入框中禁止编辑
+        text.setDisable(true);
+        text.setPrefWidth(width / 2 - 200);
+
+        // 点击按钮，选择文件夹
+        Button buttonFileChoose = new Button("选择文件夹路径");
+        buttonFileChoose.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DirectoryChooser dc = new DirectoryChooser();
+                File file = dc.showDialog(primaryStage);
+                String path = file.getPath();
+                text.setText(path);
             }
         });
 
-        GridPane inputGridPane = new GridPane();
-        GridPane.setConstraints(openButton, 0, 0);
-        inputGridPane.setHgap(6);
-        inputGridPane.setVgap(6);
-        inputGridPane.getChildren().addAll(openButton);
+        // 横向布局
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(buttonFileChoose, text);
+        hBox.setSpacing(20);
 
-        Pane rootGroup = new VBox(12);
-        rootGroup.getChildren().addAll(inputGridPane);
-        rootGroup.setPadding(new Insets(12, 12, 12, 12));
 
-        Scene scene = new Scene(rootGroup, 640, 360);
+        Label label1 = new Label();
+        label1.setText("旧字符");
+        TextField oldFiled = new TextField();
 
-        stage.setScene(scene);
-        stage.setTitle("HelloFx Maven Test");
-        stage.show();
+        Label label2 = new Label();
+        label2.setText("新字符");
+        label2.setStyle("");
+        TextField newFiled = new TextField();
+
+        HBox hBox1 = new HBox();
+        hBox1.setLayoutX(0);
+        hBox1.setLayoutY(30);
+        hBox1.getChildren().addAll(label1, oldFiled, label2, newFiled);
+        hBox1.setSpacing(10);
+
+        Button edit = new Button("批量修改");
+        TextArea ta = new TextArea();
+        ta.setDisable(true);
+        VBox hBox2 = new VBox();
+        hBox2.setLayoutX(0);
+        hBox2.setLayoutY(60);
+        hBox2.getChildren().addAll(edit, ta);
+        hBox2.setSpacing(10);
+
+        edit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (text.getText() != "" && oldFiled.getText() != "" && newFiled.getText() != "") {
+                    ReName.replaceFileName(text.getText(), oldFiled.getText(), newFiled.getText());
+                    ta.setText("批量修改完成");
+                }
+            }
+        });
+
+
+        // 布局类
+        AnchorPane root = new AnchorPane();
+        root.getChildren().addAll(hBox, hBox1, hBox2);
+
+        // 场景配置
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+
+        primaryStage.setTitle("文件批处理工具");
+        primaryStage.setWidth(width / 2);
+        primaryStage.setHeight(height / 2);
+        primaryStage.setMinWidth(width / 2);
+        primaryStage.setMinHeight(height / 2);
+        primaryStage.setMaxWidth(width);
+        primaryStage.setMaxHeight(height);
+        primaryStage.show();
+
+
     }
 }
