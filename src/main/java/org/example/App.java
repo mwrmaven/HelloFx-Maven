63b2b6b866e349bff5547c6d4ac6dbd243a56828
1227156208@qq.com <1227156208@qq.com>
2021-11-10 15:03:19 +0800

    将首页改为TabPane和Tab组件，减少代码逻辑判断和手动窗口切换

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,8 @@ import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Screen;
@@ -32,32 +34,26 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        // 设置顶部区域
-        Button topB1 = new Button(Common.TOP_BUTTON_1);
-        Button topB2 = new Button(Common.TOP_BUTTON_2);
-        Button topB3 = new Button(Common.TOP_BUTTON_3);
-        Button topB4 = new Button(Common.TOP_BUTTON_4);
-        Button topB5 = new Button(Common.TOP_BUTTON_5);
-        Button topB6 = new Button(Common.TOP_BUTTON_6);
-        // 设置按钮属性
-        topB1.setPrefHeight(30);
-        topB2.setPrefHeight(30);
-        topB3.setPrefHeight(30);
-        topB4.setPrefHeight(30);
-        topB5.setPrefHeight(30);
-        // 横向布局的创建以及配置
-        HBox hb = new HBox();
-        hb.setPrefHeight(50);
-        hb.setStyle("-fx-background-color: LightGrey");
-        hb.setSpacing(20);
-        hb.setAlignment(Pos.CENTER_LEFT);
-        hb.setPadding(new Insets(10));
-        // 将按钮组件添加到横向布局中
-        hb.getChildren().addAll(topB1, topB2, topB3, topB4, topB5, topB6);
+        // 使用TabPane，页面切换
+        TabPane tp = new TabPane();
 
-        // 方位布局，并设置方位布局的顶部和中心区域
-        BorderPane bor = new BorderPane();
-        bor.setTop(hb);
+        // 页面1、2、3、4、5、6
+        Tab tab1 = new Tab(Common.TOP_BUTTON_1);
+        Tab tab2 = new Tab(Common.TOP_BUTTON_2);
+        Tab tab3 = new Tab(Common.TOP_BUTTON_3);
+        Tab tab4 = new Tab(Common.TOP_BUTTON_4);
+        Tab tab5 = new Tab(Common.TOP_BUTTON_5);
+        Tab tab6 = new Tab(Common.TOP_BUTTON_6);
+
+        // 不可关闭
+        tab1.setClosable(false);
+        tab2.setClosable(false);
+        tab3.setClosable(false);
+        tab4.setClosable(false);
+        tab5.setClosable(false);
+        tab6.setClosable(false);
+
+        tp.getTabs().addAll(tab1, tab2, tab3, tab4, tab5, tab6);
 
         // 获取屏幕的宽、高
         Screen screen = Screen.getPrimary();
@@ -66,7 +62,7 @@ public class App extends Application {
         double height = bounds.getHeight();
 
         // 场景配置
-        Scene scene = new Scene(bor);
+        Scene scene = new Scene(tp);
         primaryStage.setScene(scene);
 
         primaryStage.setTitle(Common.STAGE_TITLE);
@@ -80,6 +76,9 @@ public class App extends Application {
         primaryStage.setMaxWidth(width);
         primaryStage.setMaxHeight(height);
         primaryStage.show();
+
+        // 默认选中第一页
+        tp.getSelectionModel().select(tab1);
 
         // 基本的文件名字符替换
         AnchorPane replacePane = new ReplacePane().replacePane(primaryStage, width);
@@ -95,36 +94,15 @@ public class App extends Application {
         // 公众号文章中音视频下载
         AnchorPane downloadDialog = new DownLoadMediaFromArticle().download(primaryStage, width, h);
 
-        bor.setCenter(downloadDialog);
-        Map<Button, Pane> buttonAndPane = new HashMap<>();
-        buttonAndPane.put(topB1, replacePane);
-        buttonAndPane.put(topB2, increaseIdentification);
-        buttonAndPane.put(topB3, ctf);
-        buttonAndPane.put(topB4, stl);
-        buttonAndPane.put(topB5, urlConvert);
-        buttonAndPane.put(topB6, downloadDialog);
+        tab1.setContent(replacePane);
+        tab2.setContent(increaseIdentification);
+        tab3.setContent(ctf);
+        tab4.setContent(stl);
+        tab5.setContent(urlConvert);
+        tab6.setContent(downloadDialog);
 
 
-        // 配置方位布局类的中心位置触发事件
-        setBorderPaneCenter(buttonAndPane, bor);
     }
-
-    private void setBorderPaneCenter(Map<Button, Pane> buttonAndPane, BorderPane bor) {
-        // 遍历配置点击按钮触发对应的事件，切换中心区域
-        buttonAndPane.forEach((k, v) -> {
-            k.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    if (!v.equals(bor.getCenter())) {
-                        System.out.println("切换到: " + k.getText());
-                        bor.setCenter(v);
-                    }
-                }
-            });
-        });
-    }
-
-
 
 
 }
