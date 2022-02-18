34bc8500589f619be8874c5c9140ce80be686fbc
1227156208@qq.com <1227156208@qq.com>
2022-02-18 17:09:42 +0800

    基本完成重构文件比对功能

--- src/main/java/org/example/interfaces/impl/FileCompare.java
+++ src/main/java/org/example/interfaces/impl/FileCompare.java
@@ -1,5 +1,6 @@
 package org.example.interfaces.impl;
 
+import com.mavenr.encrypt.MD5;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -13,6 +14,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.ooxml.POIXMLDocument;
 import org.apache.poi.ooxml.extractor.POIXMLTextExtractor;
@@ -22,9 +25,11 @@ import org.example.button.BatchButton;
 import org.example.common.Common;
 import org.example.interfaces.Function;
 import org.example.util.Unit;
+import org.fxmisc.richtext.InlineCssTextArea;
 
 import java.io.*;
-import java.util.List;
+import java.nio.charset.Charset;
+import java.util.*;
 
 /**
  * @author mavenr
@@ -76,7 +81,7 @@ public class FileCompare  implements Function {
         vBoxLeft.getChildren().add(chooseFile);
 
         // 左侧显示文本内容区域
-        TextArea newTextArea = new TextArea();
+        InlineCssTextArea newTextArea = new InlineCssTextArea();
         newTextArea.setPrefHeight(600);
         vBoxLeft.getChildren().add(newTextArea);
 
@@ -96,7 +101,7 @@ public class FileCompare  implements Function {
         vBoxRight.getChildren().add(chooseOldFile);
 
         // 右侧显示文本内容区域
-        TextArea oldTextArea = new TextArea();
+        InlineCssTextArea oldTextArea = new InlineCssTextArea();
         oldTextArea.setPrefHeight(600);
         vBoxRight.getChildren().add(oldTextArea);
 
@@ -136,7 +141,7 @@ public class FileCompare  implements Function {
                 String oldContent = getContent(oldField.getText(), oldTextArea);
 
                 // 比较两边的文本
-
+                compareString(newContent, oldContent, newTextArea);
             }
         });
 
@@ -149,10 +154,11 @@ public class FileCompare  implements Function {
      * @param textArea 文本内容区域
      * @return 文件内容
      */
-    public String getContent(String filePath, TextArea textArea) {
+    public String getContent(String filePath, InlineCssTextArea textArea) {
         // 判断新文件格式
         if (!filePath.endsWith(".txt") && !filePath.endsWith(".doc") && !filePath.endsWith(".docx")) {
-            textArea.setText("当前只能匹配txt格式或doc、docx格式文件");
+            textArea.clear();
+            textArea.appendText("当前只能匹配txt格式或doc、docx格式文件");
             return "";
         }
 
@@ -171,7 +177,9 @@ public class FileCompare  implements Function {
                     sb.append(line).append("\n");
                     line = br.readLine();
                 }
-                textArea.setText(sb.toString());
+//                textArea.setText(sb.toString());
+                textArea.clear();
+                textArea.appendText(sb.toString());
                 return sb.toString();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -194,7 +202,9 @@ public class FileCompare  implements Function {
                 fis = new FileInputStream(file);
                 WordExtractor word = new WordExtractor(fis);
                 String content = word.getText();
-                textArea.setText(content);
+//                textArea.setText(content);
+                textArea.clear();
+                textArea.appendText(content);
                 return content;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -212,12 +222,121 @@ public class FileCompare  implements Function {
                 OPCPackage opcPackage = POIXMLDocument.openPackage(filePath);
                 POIXMLTextExtractor extractor = new XWPFWordExtractor(opcPackage);
                 String content = extractor.getText();
-                textArea.setText(content);
+//                textArea.setText(content);
+                textArea.clear();
+                textArea.appendText(content);
                 return content;
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return "";
+    }
+
+    /**
+     * 比较不同的行，并在文本区域显示出来
+     * @param newContent
+     * @param oldContent
+     * @param newTextArea
+     */
+    public void compareString(String newContent, String oldContent, InlineCssTextArea newTextArea) {
+        List<List<String>> newList = getMD5AndResource(newContent);
+        List<List<String>> oldList = getMD5AndResource(oldContent);
+
+        if (CollectionUtils.isNotEmpty(newList) && CollectionUtils.isNotEmpty(oldList)) {
+            // 正向遍历，找出不同
+            List<String> newMD5List = newList.get(0);
+            List<String> oldMD5List = oldList.get(0);
+            int newSize = newMD5List.size();
+            int oldSize = oldMD5List.size();
+            int count = Math.min(newSize, oldSize);
+            int flag = -1;
+            for (int i = 0; i < count; i++) {
+                String newMD5 = newMD5List.get(i);
+                String oldMD5 = oldMD5List.get(i);
+                if (!newMD5.equals(oldMD5)) {
+                    flag = i;
+                    break;
+                }
+            }
+            // 根据flag来设置不同部分（以红色表示）
+            if (flag == -1) {
+                return;
+            }
+
+            // 注意，这里的newContent的编码不明确，所以暂不使用 newContent.length - 1 求end
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < flag; i++) {
+                start += newList.get(1).get(i).length() + 1;
+                end += newList.get(1).get(i).length() + 1;
+            }
+            // 判断具体的开始位置
+            String newLine = newList.get(1).get(flag);
+            String oldLine = oldList.get(1).get(flag);
+            for (int i = 0; i < Math.min(newLine.length(), oldLine.length()); i++) {
+                if (newLine.charAt(i) == oldLine.charAt(i)) {
+                    start += 1;
+                } else {
+                    break;
+                }
+            }
+
+            for (int i = flag; i < newSize; i++) {
+                end += newList.get(1).get(i).length() + 1;
+            }
+            end -= 1;
+
+            newTextArea.setStyle(start, end, "-fx-fill: red");
+        }
+    }
+
+    /**
+     * 获取每行数据对应的md5码，以及每行数据的原数据
+     * @param content
+     * @return
+     */
+    private List<List<String>> getMD5AndResource(String content) {
+        List<List<String>> result = new ArrayList<>();
+        if (StringUtils.isEmpty(content)) {
+            return result;
+        }
+        ByteArrayInputStream byteArrayInputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader br = null;
+        // 读取每行数据
+        try {
+            byteArrayInputStream = new ByteArrayInputStream(content.getBytes(Charset.forName("utf-8")));
+            inputStreamReader = new InputStreamReader(byteArrayInputStream);
+            br = new BufferedReader(inputStreamReader);
+
+            List<String> md5List = new ArrayList<>();
+            List<String> resource = new ArrayList<>();
+            String line = br.readLine();
+            while (line != null) {
+                md5List.add(MD5.encode(line));
+                resource.add(line);
+                line = br.readLine();
+            }
+            result.add(md5List);
+            result.add(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (byteArrayInputStream != null) {
+                    byteArrayInputStream.close();
+                }
+                if (inputStreamReader != null) {
+                    inputStreamReader.close();
+                }
+                if (br != null) {
+                    br.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
