9e1013d53d88440b6bb88b77ee6e289ccd9a9fd8
马文瑞 <mawenrui@mawenruideMacBook-Pro.local>
2022-07-26 22:28:54 +0800

    url转换功能中获取页面价格部分只获取 msg——goods——scPrice的价格

--- src/main/java/org/example/interfaces/impl/UrlConvert.java
+++ src/main/java/org/example/interfaces/impl/UrlConvert.java
@@ -1423,19 +1423,19 @@ public class UrlConvert implements Function {
         } catch (JSONException e) {
             return object.getString("msg");
         }
-        double mbPrice = msg.getJSONObject("goodsStock").getDoubleValue("salePrice");
+//        double mbPrice = msg.getJSONObject("goodsStock").getDoubleValue("salePrice");
         double scPrice = msg.getJSONObject("goods").getDoubleValue("scPrice");
         // 会员价格
-        double memberPrice = msg.getJSONObject("goodsStock").getDoubleValue("memberPrice");
+//        double memberPrice = msg.getJSONObject("goodsStock").getDoubleValue("memberPrice");
 
-        System.out.println("mbPrice=" + mbPrice);
+//        System.out.println("mbPrice=" + mbPrice);
         System.out.println("scPrice=" + scPrice);
-        System.out.println("memberPrice=" + memberPrice);
+//        System.out.println("memberPrice=" + memberPrice);
 
-        StringBuilder result = new StringBuilder("页面显示价格：").append(Math.max(mbPrice, scPrice));
-        if (memberPrice != 0) {
-            result.append("; 会员价格：").append(memberPrice);
-        }
+        StringBuilder result = new StringBuilder("页面显示价格：").append(scPrice);
+//        if (memberPrice != 0) {
+//            result.append("; 会员价格：").append(memberPrice);
+//        }
         return result.toString();
     }
 
