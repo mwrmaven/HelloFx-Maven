5abf6db9176ff36df7af07d65bd17be57da655d9
1227156208@qq.com <1227156208@qq.com>
2023-04-07 14:03:20 +0800

    将自动化处理功能中的报错停止程序逻辑删除，即使报错，也先生成数据文件

--- src/main/java/org/example/util/Unit.java
+++ src/main/java/org/example/util/Unit.java
@@ -15,6 +15,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.example.init.Config;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -193,6 +194,41 @@ public class Unit {
                 }
             }
         });
+    }
+
+    /**
+     * 启动浏览器
+     * @param chromePath
+     * @param ta
+     */
+    public void startChrome(String chromePath, TextArea ta) {
+        if (StringUtils.isBlank(chromePath)) {
+            ta.setText("请输入chrome浏览器的启动器类的文件路径！");
+            return;
+        }
+        // 获取文件工具的路径
+        String currentPath = System.getProperty("user.dir");
+        // chrome测试数据存放路径
+        String chromeTestPath= currentPath + File.separator + "chromeTest";
+        // 启动chrome调试
+        ta.appendText("\nchromePath = " + chromePath);
+        // 查看端口是否被占用，如果被占用则先停掉端口再启动 区分 windows 和 mac ，命令行也是
+        boolean alive = SocketUtil.isAlive("127.0.0.1", 9527);
+        if (alive) {
+            ta.setText("测试浏览器已启动，请在任务栏中点击浏览器图标创建新窗口！");
+            return;
+        }
+        String[] cmd = new String[3];
+        cmd[0] = chromePath;
+        cmd[1] = "--remote-debugging-port=9527";
+        cmd[2] = "--user-data-dir=" + chromeTestPath;
+        try {
+            Runtime.getRuntime().exec(cmd);
+            ta.appendText("\nchrome浏览器远程调试模式启动成功！");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            ta.appendText("\nchrome浏览器远程调试模式启动失败，请联系技术人员！");
+        }
     }
 
 }
